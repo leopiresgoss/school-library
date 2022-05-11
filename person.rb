382261e75daf4191699bf02224101f9e3ea43dde
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-05-10 19:48:35 -0600

    Added Timmer and Capitalize decorators

--- person.rb
+++ person.rb
@@ -1,5 +1,10 @@
 require 'time'
-require_relative 'nameable'
+
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -29,3 +34,42 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+# base decorator
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+# decorators
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    super()
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    super()
+    @nameable.correct_name[0, 9]
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+puts "person: #{person.correct_name}"
+# => person: maximilianus
+
+capitalize_person = CapitalizeDecorator.new(person)
+puts "capitalizedPerson: #{capitalize_person.correct_name}"
+# => capitalizedPerson: Maximilianus
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalize_person)
+puts "capitalized_trimmed_person: #{capitalized_trimmed_person.correct_name}"
+# => capitalized_trimmed_person: Maximilia
