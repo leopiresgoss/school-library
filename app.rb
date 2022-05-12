169f9790ef641ac80c98db771504cd543a7b63b3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-05-12 14:24:15 -0600

    Add methods to add a book and display all books

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
+require './book'
+
 class App
   def initialize
     @options = ['List all books', 'List all people', 'Create a person', 'Create a book',
                 'Create a rental', 'List all rentals for a given person id', 'Exit']
+    @books = []
   end
 
   def run
@@ -9,6 +12,7 @@ class App
   end
 
   def display_options
+    puts ''
     puts 'Please, choose an option by entering a number:'
     @options.each_with_index { |option, index| puts "#{index + 1} - #{option}" }
     select_option
@@ -16,6 +20,31 @@ class App
 
   def select_option
     option = gets.chomp.to_i
-    display_options if option < 1 || option > 7
+    case option
+    when 1
+      list_all_books
+    when 4
+      create_book
+    when 7
+      puts 'Thanks you for using this app!'
+    else
+      display_options
+    end
+  end
+
+  def list_all_books
+    @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    display_options
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book created successfully'
+    display_options
   end
 end
--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@ require './app'
 
 def main
   puts 'Welcome to School Library App!'
-  puts ''
   app = App.new
   app.run
 end
